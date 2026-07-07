57bade2890dad93803b363291169b8b343499840
darshan15071999 <103839904+darshan15071999@users.noreply.github.com>
2026-07-07 12:36:50 +0530

    metafix

--- src/pages/Spoch.tsx
+++ src/pages/Spoch.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Check, Mail } from 'lucide-react';
 import './Spoch.css';
 import { useWaitlist } from '../context/WaitlistContext';
